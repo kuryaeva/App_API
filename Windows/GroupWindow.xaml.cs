cc852e76073bf2ae254b315d081b00f78eb69399
Acer <Acer@DESKTOP-GG5PIOL>
2023-03-22 12:39:35 +0300

    Добавьте файлы проекта.

---
+++ Windows/GroupWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App_API.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для GroupWindow.xaml
+    /// </summary>
+    public partial class GroupWindow : Window
+    {
+        public GroupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
